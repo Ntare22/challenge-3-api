167a909c99a7e226e73722470ec40de89bb00cb8
Jim Ntare <jntare@Jims-BK-MacBook-Pro.local>
2021-07-20 10:14:42 +0200

    finshed implementing the feature

--- src/album/album.controller.ts
+++ src/album/album.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { Album } from './album.entity';
+import { AlbumService } from './album.service';
 
 @Controller('album')
-export class AlbumController {}
+export class AlbumController {
+    constructor(private albumService: AlbumService) {}
+
+    @Get('/:id/photos')
+    getPhotosById(
+        @Param('id') id: string
+    ): Promise<Album[]> {
+        return this.albumService.getPhotosById(id);
+    }
+}
--- src/album/album.module.ts
+++ src/album/album.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AlbumService } from './album.service';
 import { AlbumController } from './album.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AlbumRepository } from './album.repository';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([AlbumRepository])
+  ],
   providers: [AlbumService],
   controllers: [AlbumController]
 })
