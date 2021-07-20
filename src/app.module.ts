d89279b935d89762be3e7e652a03f0620ba6a686
Jim Ntare <jntare@Jims-BK-MacBook-Pro.local>
2021-07-20 10:58:42 +0200

    correct deployment

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,7 +12,12 @@ import { AlbumModule } from './album/album.module';
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: async (configService: ConfigService) => {
+        const isProduction = configService.get('NODE_ENV') === 'production';
         return {
+          ssl: isProduction,
+          extra: {
+            ssl: isProduction ? { rejectUnauthorized: false } : null
+          },
           type: 'postgres',
           autoLoadEntities: true,
           synchronize: true,
