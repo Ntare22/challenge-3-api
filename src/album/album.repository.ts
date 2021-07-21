b3a5f5cdc6bb56a83e73cb26b9fbd0590db07f46
Jim Ntare <jntare@Jims-BK-MacBook-Pro.local>
2021-07-21 08:56:53 +0200

    get photo endpoint

--- src/album/album.repository.ts
+++ src/album/album.repository.ts
@@ -1,10 +1,25 @@
 import { EntityRepository, Repository } from "typeorm";
 import { Album } from './album.entity'
+import { CreatePhotoDto } from "./dto/create.photo.dto";
 
 @EntityRepository(Album)
 export class AlbumRepository extends Repository<Album> {
     async getPhotos(id: string): Promise<Album[]> {
         const photos = this.find();
         return photos;
+    }
+
+    async createPhoto(createPhotoDto: CreatePhotoDto): Promise<Album> {
+        const { albumId, title, url, thumbnailUrl } = createPhotoDto;
+
+        const photo = this.create({
+            albumId,
+            title,
+            url,
+            thumbnailUrl
+        });
+
+        await this.save(photo);
+        return photo;
     }
 }
